2b6f0ab490dcea51400cbec0cea682abfa96c559
Megagig <megagigsoftwaresolutions.com>
2024-06-17 17:14:02 +0100

    Implement Darkmode on the Header without LocalStorage

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,7 +1,17 @@
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/logo.png';
+import { useEffect, useState } from 'react';
 
 export const Header = () => {
+  const [darkmode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkmode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkmode]);
   return (
     <header>
       <nav className="bg-white dark:bg-gray-900">
@@ -13,7 +23,10 @@ export const Header = () => {
             </span>
           </Link>
           <div className="flex items-center relative">
-            <span className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-gear-wide-connected"></span>
+            <span
+              onClick={() => setDarkMode(!darkmode)}
+              className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-gear-wide-connected"
+            ></span>
             <span className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-search"></span>
             <Link to="/cart" className="text-gray-700 dark:text-white mr-5">
               <span className="text-2xl bi bi-cart-fill relative">
