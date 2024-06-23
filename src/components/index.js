879d97400da7015ab530c0b5ded4316378ebf9f0
Megagig <megagigsoftwaresolutions.com>
2024-06-23 13:03:46 +0100

    Implement Scroll to top

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { Header } from './Layouts/Header';
 export { Footer } from './Layouts/Footer';
 
 export { ProductCard } from './Elements/ProductCard';
+
+export { ScrollToTop } from './Others/ScrollToTop';
